72a5f2793524a48d37ff24bf4a3dd532a57c7700
brandonCastaneda5201 <brandon.castaneda5201@alumnos.udg.mx>
2024-11-07 16:32:02 -0600

    Testeo de vue: Mr. stark i dont feel so good

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="personaje in personajesArray" :key="personaje.id">
+    <p>Nombre: {{personaje.name}}</p>
+    <p>especie: {{personaje.species}}</p>
+    <p>Origen: {{personaje.origin.name}}</p>
+    <button v-on:click="eliminar(personaje.id)">Eliminar</button>
+  </div>
+      <button v-on:click="agregar()">HELP ME!!!!</button>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script setup>
+import axios from "axios";
+import {ref, onMounted} from "vue";
+  const personajesArray = ref([]);
+  const eliminar = id => {
+    personajesArray.value = personajesArray.value.filter((personaje) => personaje.id !== id);
   }
-}
+  const agregar = () => {
+    const personajeNuevo = {
+      name: "personaje xd",
+      id: "1",
+      origin: {
+        name: "Nose"
+      },
+      species: "Yes",
+    }
+    personajesArray.value.push(personajeNuevo);
+  }
+
+onMounted(() => {
+  axios("https://rickandmortyapi.com/api/character").then(res => {
+    personajesArray.value = res.data.results;
+    console.log(personajesArray);
+});
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
